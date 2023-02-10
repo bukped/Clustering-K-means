5fa2a8b1984423b6f073b30f7b3976b3e748eddd
lina1302 <59255829+lina1302@users.noreply.github.com>
2023-02-10 11:51:29 +0700

    library machine learning

---
+++ Chapter 2/library.py
@@ -0,0 +1,23 @@
+# Library numpy untuk mengelolah data angka dalam operas matriks dan vektor
+import numpy as np
+
+# library pandas untuk membaca, mengelola, dan mengubah data dengan join, group by, dll
+import pandas as pd
+
+# library visualisasi data, membuat lebel data
+import matplotlib.pyplot as plt
+
+# library visualisasi data
+import seaborn as sns
+
+# library untuk mengelola data dengan algoritma machine learning
+import sklearn
+
+# MinMaxScaler untuk membuat rentangan data 0-1
+from sklearn.preprocessing import MinMaxScaler
+
+# library untuk menggunakan algoritma k-means
+from sklearn.cluster import KMeans
+
+# library untuk memvalidasi konsistensi kelompok data
+from sklearn.metrics import silhouette_score
